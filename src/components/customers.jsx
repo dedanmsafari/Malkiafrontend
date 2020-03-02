5d926a8167dc1172a3b624cd4daac4e2d473c540
Dedan Msafari <dedan.msafari@gmail.com>
2020-03-02 15:07:16 +0300

    minor changes to customers.jsx

--- src/components/customers.jsx
+++ src/components/customers.jsx
@@ -1,6 +1,17 @@
 import React, { Component } from 'react';
+import { getCustomers } from "../services/customerService";
 class Customers extends Component {
-  state = {  }
+  state = { 
+    customers:[]
+   }
+
+   async componentDidMount() {
+    const {data:customers} = await getCustomers();
+    this.setState({customers});
+    console.log(customers);
+    
+    
+  }
   render() { 
     return ( 
       <p>Customers</p>
