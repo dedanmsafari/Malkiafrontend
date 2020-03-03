cf3b6a8f99bfb06ae4c7ac3653f2df32f7dda9c9
Dedan Msafari <dedan.msafari@gmail.com>
2020-03-03 12:03:24 +0300

    modified customer module

--- src/components/customers.jsx
+++ src/components/customers.jsx
@@ -1,22 +1,28 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 import { getCustomers } from "../services/customerService";
 class Customers extends Component {
-  state = { 
-    customers:[]
-   }
+  state = {
+    customers: []
+  };
 
-   async componentDidMount() {
-    const {data:customers} = await getCustomers();
-    this.setState({customers});
+  async componentDidMount() {
+    const { data: customers } = await getCustomers();
+    this.setState({ customers });
     console.log(customers);
-    
-    
   }
-  render() { 
-    return ( 
-      <p>Customers</p>
-     );
+  render() {
+    const { customers } = this.state;
+    return customers.map(c => (
+      
+      <button className="list-group-item list-group-item-action" key={c._id}>
+        <div className="d-flex w-100 justify-content-between">
+          <h5 className="mb-1">{c.name}</h5>
+        </div>
+        <p className="mb-1">Phone No:{c.phone}</p>
+        <small>Status:{c.isGold}</small>
+      </button>
+    ));
   }
 }
- 
-export default  Customers;
+
+export default Customers;
