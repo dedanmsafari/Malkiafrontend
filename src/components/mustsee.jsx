224296b1e8bda172671848bec4f79478fd71a5d8
Dedan Msafari <dedan.msafari@gmail.com>
2020-03-12 12:57:01 +0300

    added a must see  component

--- src/components/mustsee.jsx
+++ src/components/mustsee.jsx
@@ -4,26 +4,42 @@ class MustSee extends Component {
 
     state = {
         movies:[]
+
     }
     
     async componentDidMount() {
         const { data: movies } = await getMovies();
         this.setState({ movies });
-        console.log(movies);
+    
         
       }
       getPopMovies = () => {
-          const {movies} = this.state;
-          if (movies.some(movie => movie.dailyRentalRate <= 5))
-          this.setState({movies});
-          console.log(movies);
+      
+const popular = this.state.movies.filter( m => m.dailyRentalRate >= 5)
+        return  { liked:popular }
           
       }
     render() { 
-        const {movies} = this.state;
-            return movies.map(m => (
-                    <h1>{m.title}</h1>
-            ))
+        const {liked:popular} = this.getPopMovies();
+        console.log(popular);
+        
+            return ( 
+                <React.Fragment>
+                    <h5>Our System detect that the following are the most watched shows.
+                     Visit your nearest rental shop and grab a copy</h5>
+                   { popular.map(p => (
+                        <button className="list-group-item list-group-item-action" key={p._id}>
+                        <div className="d-flex w-100 justify-content-between">
+                          <h5 className="mb-1">{p.title}</h5>
+                        </div>
+                        <p className="mb-1">numberinStock:{p.numberInStock}</p>
+                        <p>description:{p.description}</p>
+                        <small>RentalRate:{p.dailyRentalRate}</small>
+                      </button>
+                ))};
+
+                </React.Fragment>
+            )
     }     
 }
  
