22ecc4719dc00af7a856bc98956c2f8335b8ace9
Dedan Msafari <dedan.msafari@gmail.com>
2020-03-28 12:24:19 +0300

    modified the rentalService Route

--- src/services/rentalService.js
+++ src/services/rentalService.js
@@ -1,7 +1,27 @@
 import http from "./httpService";
 import {apiEndpoint} from "../config";
 
+function rentalUrl(rentalId) {
+    return (`${apiEndpoint}/rentals/${rentalId}`)
+}
+function returnUrl(rentalId) {
+    return (`${apiEndpoint}/returns/${rentalId}`)
+}
 export function getRentals() {
     return http.get(apiEndpoint + '/rentals');
 }
-export default { getRentals };
+export function saveRental(rental) {
+    if (rental._id) {
+        const body = {
+            ...rental
+        };
+        delete body._id;
+        return http.put(rentalUrl(rental._id), body);
+
+    }
+    return http.post(apiEndpoint + '/rentals', rental);
+}
+
+export function getRental(rentalId) {
+    return http.get(rentalUrl(rentalId));
+}
