4bd001578ac9aeac33568878c83682559e65c2f0
Dedan Msafari <dedan.msafari@gmail.com>
2020-03-29 14:52:08 +0300

    added customer service route

--- src/services/customerService.js
+++ src/services/customerService.js
@@ -1,7 +1,23 @@
 import http from "./httpService";
 import {apiEndpoint} from "../config";
 
+function customerUrl(customerId) {
+    return (`${apiEndpoint}/customers/${customerId}`)
+}
+export function saveCustomer(customer) {
+    if (customer._id) {
+        const body = {
+            ...customer
+        };
+        delete body._id;
+        return http.put(customerUrl(customer._id), body);
+
+    }
+    return http.post(apiEndpoint + '/customers',customer);
+}
 export function getCustomers() {
     return http.get(apiEndpoint + '/customers');
 }
-export default { getCustomers };
+export function getCustomer(customerId) {
+    return http.get(customerUrl(customerId));
+}
