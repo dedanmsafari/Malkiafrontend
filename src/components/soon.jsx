611c3672fb6377ed967fa67f31680dd8177c9294
Dedan Msafari <dedan.msafari@gmail.com>
2020-05-14 04:16:54 +0300

    a future soon component

---
+++ src/components/soon.jsx
@@ -0,0 +1,88 @@
+import React, { Component } from 'react'
+class Soon extends Component {
+    state = {  }
+    render() { 
+        return (
+            <React.Fragment> 
+                  {/* <div className="sectionLight">
+      <div className="row">
+        <div className="col-sm-12">
+          <div className="row">
+            <div className="col-sm-12 col-lg-4">
+              <img className="img-fluid imgCenter" src="aset/c9.jpg" alt="African child toys"/>
+              <div className="row">
+                <div className="col-sm-12">
+                  <div className="buttonwrapper">
+                    <button type="button" className="btn btn-lg btn-primary coolbtncol" data-toggle="modal"
+                      data-target=".cooltoy-modal-lg">cool toys</button>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="col-sm-12 col-md-4">
+              <img className="img-fluid  imgCenter" src="aset/c11.jpg" alt="Cool Trends"/>
+              <div className="row">
+                <div className="col-sm-12">
+                  <div className="buttonwrapper">
+                    <button className="btn btn-lg btn-primary coolbtncol" data-toggle="modal"
+                      data-target=".trendcloth-modal-lg">Trending clothes</button>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="col-sm-12 col-md-4">
+              <img className="img-fluid imgCenter" src="aset/c10.jpg" alt="Amazing Outlook" />
+              <div className="row">
+                <div className="col-sm-12">
+                  <div className="buttonwrapper">
+                    <button className="btn btn-lg btn-primary coolbtncol" data-toggle="modal"
+                      data-target=".style-modal-lg">Stylish looks</button>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+            <div classNameName="modal fade cooltoy-modal-lg" tabIndex="-1" role="dialog" aria-labelledby="myLargeModalLabel"
+            aria-hidden="true">
+            <div classNameName="modal-dialog modal-lg" role="document">
+              <div classNameName="modal-content">
+                <div classNameName="embed-responsive embed-responsive-16by9">
+                  <iframe classNameName="embed-responsive-item" src="https://www.youtube.com/embed/sifKIrzE1gk"
+                    allowFullScreen></iframe>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div classNameName="modal fade trendcloth-modal-lg" tabIndex="-1" role="dialog" aria-labelledby="myLargeModalLabel"
+            aria-hidden="true">
+            <div classNameName="modal-dialog modal-lg" role="document">
+              <div classNameName="modal-content">
+                <div classNameName="embed-responsive embed-responsive-16by9">
+                  <iframe classNameName="embed-responsive-item" src="https://www.youtube.com/embed/K4aQYhLuHrM"
+                    allowFullScreen></iframe>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div classNameName="modal fade style-modal-lg" tabIndex="-1" role="dialog" aria-labelledby="myLargeModalLabel"
+            aria-hidden="true">
+            <div classNameName="modal-dialog modal-lg" role="document">
+              <div classNameName="modal-content">
+                <div classNameName="embed-responsive embed-responsive-16by9">
+                  <iframe classNameName="embed-responsive-item" src="https://www.youtube.com/embed/c7cglxw-jss"
+                    allowFullScreen></iframe>
+                </div>
+              </div>
+            </div> */}
+          {/* </div> */}
+          <h1>Hello</h1>
+          </ React.Fragment> 
+         );
+    }
+}
+ 
+export default Soon;
