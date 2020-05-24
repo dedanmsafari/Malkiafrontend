225698d2fbaec976e53eab723b1f1d935f1f344b
Dedan Msafari <dedan.msafari@gmail.com>
2020-05-24 19:26:02 +0300

    minor change in customers

--- src/components/customers.jsx
+++ src/components/customers.jsx
@@ -1,9 +1,11 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
+import SearchBox from "./searchBox";
 import { getCustomers } from "../services/customerService";
 class Customers extends Component {
   state = {
-    customers: []
+    customers: [],
+    searchQuery: ""
   };
 
   async componentDidMount() {
@@ -11,8 +13,28 @@ class Customers extends Component {
     this.setState({ customers });
     console.log(customers);
   }
+  handleSearch = query => {
+    this.setState({ searchQuery: query });
+  };
+
+  getPagedData = () => {
+    const {
+      searchQuery,
+      customers: allCustomers
+    } = this.state;
+
+    let filtered = allCustomers;
+    if (searchQuery)
+      filtered = allCustomers.filter(r =>
+        r.name.toLowerCase().startsWith(searchQuery.toLowerCase())
+      );
+    
+    return { totalCount: filtered.length, data: filtered };
+  };
+  
   render() {
-    const { customers } = this.state;
+    const { searchQuery} = this.state;
+    const { data:filtered } = this.getPagedData();
     return (
       <React.Fragment>
            <section className="banner-main">
@@ -23,7 +45,8 @@ class Customers extends Component {
           >
             Add Customer
           </Link> }
-    {customers.map(c => (
+          <SearchBox value={searchQuery} onChange={this.handleSearch} />
+    {filtered.map(c => (
       <Link to={`/customers/${c._id}`} className="list-group-item list-group-item-action" key={c._id}>
         <div className="d-flex w-100 justify-content-between">
           <h5 className="mb-1">{c.name}</h5>
