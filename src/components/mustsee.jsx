37f6c929aecb6eaf98e1b2d285a8c5dc9fa0c69e
Dedan Msafari <dedan.msafari@gmail.com>
2020-05-24 17:54:39 +0300

    modified must see component

--- src/components/mustsee.jsx
+++ src/components/mustsee.jsx
@@ -1,22 +1,75 @@
 import React, { Component } from "react";
+import SearchBox from "./searchBox";
+import _ from "lodash";
+import MustseeTable from "./mustseeTable"
+import Pagination from "./common/pagination";
+import { paginate } from "../utils/paginate";
 import { getMovies } from "../services/movieService";
 class MustSee extends Component {
   state = {
-    movies: []
+    movies: [],
+    searchQuery: "",
+    currentPage: 1,
+    pageSize: 6,
+    sortColumn: { path:"title", order: "asc" }
   };
 
   async componentDidMount() {
     const { data: movies } = await getMovies();
     this.setState({ movies });
   }
-  getPopMovies = () => {
-    const popular = this.state.movies.filter(m => m.dailyRentalRate >= 5);
-    return { liked: popular };
+  handlePageChange = page => {
+    this.setState({ currentPage: page });
   };
-  render() {
-    const { liked: popular } = this.getPopMovies();
-    console.log(popular);
+  handleSort = sortColumn => {
+    this.setState({ sortColumn });
+  };
+  handleSearch = query => {
+    this.setState({ searchQuery: query, currentPage: 1 });
+  };
+  
+  getPagedData = () => {
+    const {
+      searchQuery,
+      sortColumn,
+      pageSize,
+      currentPage,
+      movies: allmovies
+    } = this.state;
 
+    let filtered = allmovies;
+    console.log(filtered);//all the aavailable movies
+      const best = filtered.filter(m => m.dailyRentalRate >= 13);
+console.log(best);//the ones that are the favourites
+
+
+const sorted = _.orderBy(best, [sortColumn.path], [sortColumn.order]);
+console.log(sorted);//sorting our favourite best
+
+
+let popular = paginate(sorted, currentPage, pageSize)//paginate our popular ones rename them to popular
+console.log(popular);
+
+
+if (searchQuery)
+  popular = popular.filter(p =>
+    p.title.toLowerCase().startsWith(searchQuery.toLowerCase())
+    );
+  
+
+    
+    return { totalCount: sorted.length, data: popular };
+  };
+  // getPopMovies = () => {
+  //   const popular = this.state.movies.filter(m => m.dailyRentalRate >= 5);
+  //   return { liked: popular };
+  // };
+
+  
+  render() {
+    const {totalCount ,  data: popular } = this.getPagedData();
+    console.log(totalCount);
+    const { searchQuery, pageSize, currentPage,sortColumn} = this.state;
     return (
       <React.Fragment>
         <section className="banner-main">
@@ -24,7 +77,8 @@ class MustSee extends Component {
             Our System detect that the following are the most watched shows.
             Visit your nearest rental shop and grab a copy
           </h5>
-          {popular.map(p => (
+          <SearchBox value={searchQuery} onChange={this.handleSearch} />
+          {/* {popular.map(p => (
             <a className="list-group-item list-group-item-action" key={p._id}>
               <div className="d-flex w-100 justify-content-between">
                 <h5 className="mb-1">{p.title}</h5>
@@ -36,8 +90,18 @@ class MustSee extends Component {
               <p className="mb-1">Number In Stock:{p.numberInStock}</p>
               <small>RentalRate:{p.dailyRentalRate}</small>
             </a>
-          ))}
-          ;
+          ))} */}
+          <MustseeTable
+            popular={popular}
+            sortColumn={sortColumn}
+            onSort={this.handleSort}
+          />
+        <Pagination
+          itemsCount={totalCount}
+          pageSize={pageSize}
+          currentPage={currentPage}
+          onPageChange={this.handlePageChange}
+        />
         </section>
       </React.Fragment>
     );
